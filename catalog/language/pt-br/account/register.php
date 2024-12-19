ce40f1ac7260d26ecf0bd008f77340aa6505faa2
Glauber <gau1210@gmail.com>
2024-12-19 16:13:55 -0300

    Atualizações Parciais

---
+++ catalog/language/pt-br/account/register.php
@@ -0,0 +1,7 @@
+<?php
+-->>> Caminho para chegar no arquivo dentro da pasta principal do OpenCard: catalog/language/pt-br/account/register.php
+
+Esse bloco adicionamos no final onde tem os campos de sinalização de // Error.
+
+$_['error_cpf_exists']     = 'Este CPF/CNPJ já está cadastrado!';
+?>
